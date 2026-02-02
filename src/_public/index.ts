d6933f64136f859de8048d48ab099b0f515487f4
kzoeps <kzoepa@gmail.com>
2026-02-02 12:23:32 +0600

    add changeset

--- src/_public/index.ts
+++ src/_public/index.ts
@@ -3,7 +3,7 @@ export * from "../_internal";
 import { ClimateAiSDK as SDKInternal } from "../_internal";
 
 /**
- * Pre-configured ClimateAI SDK instance for climateai.org and hypercerts.org domains.
+ * Pre-configured ClimateAI SDK instance for climateai.org and gainforest.id domains.
  *
  * Note: The getServerCaller method now requires an ATProtoSDK instance.
  * See @climateai/sdk/oauth for OAuth configuration utilities.
